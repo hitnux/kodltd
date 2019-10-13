9b12187be58a3ccfd92296301da74b5a79b9834e
hlilbilgin <hlilbilgin@gmail.com>
2019-10-13 18:06:13 +0300

    ana sayfa tasarımı

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -4,7 +4,16 @@
     <Container>
       <div class="columns">
         <Card>
-          <qr-code :text=url :size="250" color="#34495e" slot="QR"></qr-code>
+          <div slot="QR" style="display: flex; flex-direction: column;justify-content:center;position: relative;">
+          <qr-code :text=url :size="250" color="#34495e" id="qrcode"></qr-code>
+          <b-button 
+                id="refresh"
+                type="is-info"
+                icon-left="refresh"
+                v-on:click="Generate"
+                class="has-text-centered">
+            </b-button>
+          </div>
           <h2 slot="title">Hamurabi</h2>
           <p slot="content">
             İçerikler "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -30,15 +39,36 @@ export default {
   data () {
     return {
       url: "https://buefy.org",
+      url2: "https://buefy.org",
     }
   },
   components: {
     Card,
     Container
   },
+  methods:{
+    Generate: function(event){
+      this.url=this.url2+"/?v="+Math.floor(Math.random() * 10);
+    }
+  },
   created(){
   }
 }
 </script>
 <style>
+#refresh{
+  font-size: 50px;
+  width: 150px;
+  height:150px;
+  position: absolute;
+  left:50px;
+  top:50px;
+  opacity: 0;
+  border: solid 1px #0F77EA;
+  border-radius: 50%;
+  transition: opacity .5s;
+}
+#refresh:hover{
+  opacity: 1;
+}
 </style>
