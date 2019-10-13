616d0326bfbcab8b46db82f36467c61ed8a5b69f
hlilbilgin <hlilbilgin@gmail.com>
2019-10-13 17:11:27 +0300

    ana sayfa tasarımı

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -3,7 +3,7 @@
     <Container>
       <div class="columns">
         <Card>
-          <qr-code :text=url :size="250" slot="QR"></qr-code>
+          <qr-code :text=url :size="250" color="#34495e" slot="QR"></qr-code>
           <h2 slot="title">Hamurabi</h2>
           <p slot="content">
             İçerikler "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
