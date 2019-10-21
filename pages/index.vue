dae4d8f89dd3023e8216135eb87beeb72227f4b7
Muhammed Ali ÇOLAKER <m.alicolaker@gmail.com>
2019-10-21 17:50:01 +0300

    nuxtJS socket client

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,62 @@
 <template>
-<div>
-  <section class="section">
-    <PageHeader title="KodLTD" :button=true to="sender">
-      <div slot="left">
-        Yapım Aşaması: <b>Pre-Alpha Sürümü</b> Version 0.1<br>Nuxt JS, Vue JS, CSS, JS
-      </div>
-      <div id="QrLayout" slot="right">
-        <qr-code :text=url :size="250" color="#34495e" bg-color="white" id="qrcode"></qr-code>
-      </div>
-    </PageHeader>
-    <Container>
-      <div class="columns">
-        <!--<b-button 
+  <div>
+    <section class="section">
+      <PageHeader title="KodLTD" :button="true" to="sender">
+        <div slot="left">
+          Yapım Aşaması:
+          <b>Pre-Alpha Sürümü</b> Version 0.1
+          <br />Nuxt JS, Vue JS, CSS, JS
+        </div>
+        <div id="QrLayout" slot="right">
+          <qr-code :text="url" :size="250" color="#34495e" bg-color="white" id="qrcode"></qr-code>
+        </div>
+      </PageHeader>
+      <Container>
+        <div class="columns">
+          <!--<b-button 
                 type="is-info"
                 class="has-text-centered"
                 tag="router-link"
                 :to="{ name: 'inspire', params: { id: 1234 } }">
-            </b-button>-->
-      </div>
-    </Container>
-  </section>
-  <div style="background: #34495E;padding:75px;margin-bottom:var(--margin);">
-    <figure class="image">
-        <img src="~/assets/video.png" style="height:315px;width:560px;margin: auto;">
+          </b-button>-->
+        </div>
+      </Container>
+    </section>
+    <div style="background: #34495E;padding:75px;margin-bottom:var(--margin);">
+      <figure class="image">
+        <img src="~/assets/video.png" style="height:315px;width:560px;margin: auto;" />
       </figure>
+    </div>
+    <Service
+      title="Gizlilik dostu"
+      :src="require('~/assets/video.png')"
+    >İnsanların gizliliğinin temel bir hak olduğuna inanan sağlam bir şirketiz. Faaliyetlerinizi veya etkinliklerinizi izlemiyoruz.</Service>
+    <Service
+      title="Gizlilik dostu"
+      :src="require('~/assets/video.png')"
+      :reverse="true"
+    >İnsanların gizliliğinin temel bir hak olduğuna inanan sağlam bir şirketiz. Faaliyetlerinizi veya etkinliklerinizi izlemiyoruz.</Service>
+    <Service
+      title="Gizlilik dostu"
+      :src="require('~/assets/video.png')"
+    >İnsanların gizliliğinin temel bir hak olduğuna inanan sağlam bir şirketiz. Faaliyetlerinizi veya etkinliklerinizi izlemiyoruz.</Service>
+    <Reviews />
   </div>
-  <Service title="Gizlilik dostu" :src="require('~/assets/video.png')">
-    İnsanların gizliliğinin temel bir hak olduğuna inanan sağlam bir şirketiz. Faaliyetlerinizi veya etkinliklerinizi izlemiyoruz.
-  </Service>
-  <Service title="Gizlilik dostu" :src="require('~/assets/video.png')" :reverse="true">
-    İnsanların gizliliğinin temel bir hak olduğuna inanan sağlam bir şirketiz. Faaliyetlerinizi veya etkinliklerinizi izlemiyoruz.
-  </Service>
-  <Service title="Gizlilik dostu" :src="require('~/assets/video.png')">
-    İnsanların gizliliğinin temel bir hak olduğuna inanan sağlam bir şirketiz. Faaliyetlerinizi veya etkinliklerinizi izlemiyoruz.
-  </Service>
-  <Reviews/>
-</div>
 </template>
 
 <script>
-import Container from '~/components/Container'
-import PageHeader from '~/components/PageHeader'
-import Service from '~/components/Service'
-import Reviews from '~/components/Reviews'
+import Container from "~/components/Container";
+import PageHeader from "~/components/PageHeader";
+import Service from "~/components/Service";
+import Reviews from "~/components/Reviews";
+import socket from "~/plugins/socket.io";
 
 export default {
-  name: 'HomePage',
-  data () {
+  name: "HomePage",
+  data() {
     return {
-      url: "https://buefy.org",
-    }
+      url: "https://buefy.org"
+    };
   },
   components: {
     Container,
@@ -57,31 +64,31 @@ export default {
     Service,
     Reviews
   },
-  methods:{
-  },
+  methods: {},
 
-  created(){
+  created() { 
+    console.log(socket.id);   
   }
-}
+};
 </script>
 <style>
-#QrLayout{
+#QrLayout {
   display: flex;
-  justify-content:center;
+  justify-content: center;
 }
-#refresh{
+#refresh {
   font-size: 50px;
   width: 150px;
-  height:150px;
+  height: 150px;
   position: absolute;
-  left:50px;
-  top:50px;
+  left: 50px;
+  top: 50px;
   opacity: 0;
-  border: solid 1px #0F77EA;
+  border: solid 1px #0f77ea;
   border-radius: 50%;
-  transition: opacity .5s;
+  transition: opacity 0.5s;
 }
-#refresh:hover{
+#refresh:hover {
   opacity: 1;
 }
 </style>
