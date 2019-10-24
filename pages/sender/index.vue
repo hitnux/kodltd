7a67bbb3c4ebd99c75afdf070af492d2f5b6a751
hlilbilgin <hlilbilgin@gmail.com>
2019-10-24 21:06:44 +0300

    error page

--- pages/sender/index.vue
+++ pages/sender/index.vue
@@ -1,16 +1,13 @@
 <template>
 <section class="section">
-    <PageHeader title="Sender">
+    <PageHeader title="Upss..">
         <div slot="left">
-            Sender page
+            Bir hata oluştu! Lütfen karakod okutma işleminin başarıyla tamamlandığından emin olun!
         </div>
-        <div slot="right">
-            <img :src="require('~/assets/video.png')" />
+        <div slot="right" style="display:flex;justify-content: center;">
+            <div class="alert">!</div>
         </div>
     </PageHeader>
-    <Container>
-        <h1> İşlem yapabilmek için önce karekodu okutmalısın!</h1>
-    </Container>
 </section>
 </template>
 <script>
@@ -24,3 +21,17 @@ export default {
     }
 }
 </script>
+<style>
+.alert{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 100px;
+    color: white;
+    padding: 100px; 
+    height: 100px;
+    width: 100px;
+    background: red;
+    border-radius: 50%;
+}
+</style>
